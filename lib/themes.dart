19e5952833c362efaf3681d6dad0f47ab0e9535f
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-07-27 21:45:34 +0530

    refactoring

--- lib/themes.dart
+++ lib/themes.dart
@@ -12,13 +12,6 @@ class TwitterTheme {
             iconTheme: IconThemeData(color: blueTColor)),
       );
 
-  static Color blueTColor = HexColor("#1DA1F2");
-  static Color blackTColor = HexColor("#14171A");
-  static Color darkTgrey = HexColor("#657786");
-  static Color lightTgrey = HexColor("#AAB8C2");
-  static Color extraLightTgrey = HexColor("#E1E8ED");
-  static Color extraextraLightTgrey = HexColor("#F5F8FA");
-
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
         backgroundColor: Colors.black87,
@@ -27,4 +20,11 @@ class TwitterTheme {
             backgroundColor: Colors.black87,
             iconTheme: IconThemeData(color: blueTColor)),
       );
+
+  static Color blueTColor = HexColor("#1DA1F2");
+  static Color blackTColor = HexColor("#14171A");
+  static Color darkTgrey = HexColor("#657786");
+  static Color lightTgrey = HexColor("#AAB8C2");
+  static Color extraLightTgrey = HexColor("#E1E8ED");
+  static Color extraextraLightTgrey = HexColor("#F5F8FA");
 }
