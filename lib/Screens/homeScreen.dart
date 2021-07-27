f100e3eac6859ac6fad8c51c15bf81403eddc749
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-07-27 23:16:25 +0530

    Story Widget Created

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
+import 'package:twitter_clone/Widgets/Story_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,13 +13,25 @@ class HomeScreen extends StatelessWidget {
         SliverAppBar(
           backgroundColor: Theme.of(context).backgroundColor,
           centerTitle: true,
-          elevation: 5,
+          elevation: 15,
           title: Icon(
             LineIcons.twitter,
             size: 36,
           ),
           actions: [IconButton(onPressed: () {}, icon: Icon(LineIcons.star))],
         ),
+        SliverToBoxAdapter(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.1,
+            child: ListView.builder(
+              itemBuilder: (context, index) => StoryWidget(index: index),
+              physics: BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              padding: const EdgeInsets.only(left: 8),
+            ),
+          ),
+        )
       ],
     );
   }
--- lib/Widgets/Story_widget.dart
+++ lib/Widgets/Story_widget.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/themes.dart';
 
 class StoryWidget extends StatelessWidget {
-  final twitterId;
-  final imageUrl;
-  const StoryWidget({Key? key, this.twitterId, this.imageUrl}) : super(key: key);
+  // final String twitterId;
+  // final String imageUrl;
+  final int index;
+  const StoryWidget({
+    Key? key,
+    required this.index,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      child: Stack(
+        children: [
+          CircleAvatar(
+              radius: 30,
+              backgroundColor:
+                  index == 0 ? Colors.transparent : TwitterTheme.blueTColor,
+              child: CircleAvatar(
+                radius: 27,
+                backgroundImage: NetworkImage(
+                    'https://images.pexels.com/photos/2811087/pexels-photo-2811087.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+              )),
+          if (index == 0)
+            Positioned(
+              bottom: 4,
+              right: 0,
+              child: CircleAvatar(
+                backgroundColor: TwitterTheme.blueTColor,
+                radius: 10,
+                child: Icon(
+                  Icons.add,
+                  size: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
