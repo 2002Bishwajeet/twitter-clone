00b04cfdd9f242b4657d44967f688da53cad5014
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-07-30 20:40:25 +0530

    name changed of files

--- lib/providers/Create_user_provider.dart
+++ lib/providers/Create_user_provider.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
-import 'package:twitter_clone/models/UserModel.dart';
+import 'package:twitter_clone/models/CreateUserModel.dart';
 
 final firebaseStorageProvider =
     Provider<firebase_storage.FirebaseStorage>((ref) {
