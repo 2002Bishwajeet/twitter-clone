13b6b63d842484fecaaa1c98bd8bf658bc59d6cc
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2021-08-04 14:41:18 +0530

    added easterEggs

--- lib/Screens/MessageScreen.dart
+++ lib/Screens/MessageScreen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MessageScreen extends StatelessWidget {
-  const MessageScreen({ Key? key }) : super(key: key);
+  const MessageScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Center(
+      child: Image.network(
+          'https://media1.tenor.com/images/0e91293f60f1bbdebdd8a6932e4c903e/tenor.gif?itemid=18519512'),
     );
   }
 }
--- lib/Screens/NotificationScreen.dart
+++ lib/Screens/NotificationScreen.dart
@@ -5,6 +5,8 @@ class NotificationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text('Bola na , Khud Bana le'),
+    );
   }
 }
